c9224870526f7bc10134f9a13f1afc1c7cd61dcc
moha-sihab <moha.sihab@gmail.com>
2025-05-09 07:39:34 +0700

    refactor : handling error network

--- ANDROID/cram/app/src/main/java/com/mohasihab/cram/core/data/repositories/LoginRepository.kt
+++ ANDROID/cram/app/src/main/java/com/mohasihab/cram/core/data/repositories/LoginRepository.kt
@@ -5,9 +5,16 @@ import com.mohasihab.cram.core.data.remote.LoginApi
 import com.mohasihab.cram.core.data.remote.request.LoginRequest
 import com.mohasihab.cram.core.data.remote.response.BaseResponse
 import com.mohasihab.cram.core.data.remote.response.LoginResponse
+import com.mohasihab.cram.core.helper.NetworkHelper.safeApiCall
+import retrofit2.HttpException
+import java.io.IOException
 
 class LoginRepository (
     private val loginApi: LoginApi
 ) : LoginRepositoryContract {
-    override suspend fun login(loginRequest: LoginRequest): BaseResponse<LoginResponse> = loginApi.login(loginRequest)
+    override suspend fun login(loginRequest: LoginRequest): BaseResponse<LoginResponse> {
+        return safeApiCall {
+            loginApi.login(loginRequest)
+        }
+    }
 }
